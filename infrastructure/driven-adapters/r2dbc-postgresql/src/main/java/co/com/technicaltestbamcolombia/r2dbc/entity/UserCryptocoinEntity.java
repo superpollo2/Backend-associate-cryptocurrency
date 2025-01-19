d26b0fb41de85c4926adfa5697f09d19cd8a36cc
superpollo2 <laura.tascon@udea.edu.co>
2025-01-19 11:03:19 -0500

    endpoin for save a new associate

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/technicaltestbamcolombia/r2dbc/entity/UserCryptocoinEntity.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/technicaltestbamcolombia/r2dbc/entity/UserCryptocoinEntity.java
@@ -1,21 +1,21 @@
 package co.com.technicaltestbamcolombia.r2dbc.entity;
 
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Setter
 @Builder(toBuilder = true)
 @AllArgsConstructor
-@Table("UserCryptocoin")
+@NoArgsConstructor
+@Embeddable
+@Table("usercrytocoin")
 public class UserCryptocoinEntity {
 
     @Id
-    private Long id;
     private Integer userId;
+    @Id
     private Integer cryptocoinId;
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/technicaltestbamcolombia/r2dbc/repository/UserCryptocoinRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/technicaltestbamcolombia/r2dbc/repository/UserCryptocoinRepository.java
@@ -2,7 +2,11 @@ package co.com.technicaltestbamcolombia.r2dbc.repository;
 
 import co.com.technicaltestbamcolombia.r2dbc.entity.UserCryptocoinEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserCryptocoinRepository extends ReactiveCrudRepository<UserCryptocoinEntity, Integer> {
+
+    Mono<UserCryptocoinEntity> save(UserCryptocoinEntity entity);
+
 
 }
